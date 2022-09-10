424324feacdfba2597d8ef3f4b0676123908b944
Per Lundholm <per@artcomputer.se>
2022-09-10 09:59:37 +0200

    Refactored the test code to be more compact.

--- src/test/java/se/artcomputer/game/GameTest.java
+++ src/test/java/se/artcomputer/game/GameTest.java
@@ -1,5 +1,6 @@
 package se.artcomputer.game;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
@@ -14,41 +15,38 @@ import static se.artcomputer.game.GameState.RUNNING;
 
 class GameTest {
 
+    private GameInputImpl scanner;
+
+    @BeforeEach
+    void setUp() {
+        scanner = new GameInputImpl();
+    }
+
     /**
      * Here we get a bad start with damages, so we limp to the base and request reparation.
      */
     @Test
     void scenario1337() {
-        GameInputImpl scanner = new GameInputImpl();
         Game game = new Game(scanner, new Random(1337));
         game.step();
         assertEquals(RUNNING, game.gameState);
         int klingons = game.totalKlingons();
-        scanner.setLines("NAV", "2", "1");
-        game.step();
-        scanner.setLines("TOR", "4");
-        game.step();
+        command(game,"NAV", "2", "1");
+        command(game, "TOR", "4");
         assertEquals(klingons - 1, game.totalKlingons());
         Position position = game.currentSector();
-        scanner.setLines("NAV", "2", "1");
-        game.step();
+        command(game,"NAV", "2", "1");
         assertEquals(position, game.currentSector());
         for (int i = 0; i < 8; i++) {
-            scanner.setLines("NAV", "4", "0.2");
-            game.step();
+            command(game,"NAV", "4", "0.2");
         }
         assertEquals(new Position(3,6), game.currentQuadrant());
-        scanner.setLines("NAV", "3", "0.2");
-        game.step();
-        scanner.setLines("NAV", "3", "0.2");
-        game.step();
-        scanner.setLines("NAV", "5", "0.1");
-        game.step();
+        command(game,"NAV", "3", "0.2");
+        command(game,"NAV", "3", "0.2");
+        command(game,"NAV", "5", "0.1");
         assertEquals(DOCKED, game.condition());
-        scanner.setLines("DAM", "Y");
-        game.step();
-        scanner.setLines("SRS");
-        game.step();
+        command(game, "DAM", "Y");
+        command(game,"SRS");
     }
 
     /**
@@ -57,23 +55,22 @@ class GameTest {
      */
     @Test
     void scenario123456789() {
-        GameInputImpl scanner = new GameInputImpl();
         Game game = new Game(scanner, new Random(123456789));
         game.step();
-        scanner.setLines("LRS");
-        game.step();
-        scanner.setLines("NAV", "3", "1");
-        game.step();
-        scanner.setLines("SHE", "1000");
-        game.step();
+        command(game,"LRS");
+        command(game,"NAV", "3", "1");
+        command(game, "SHE", "1000");
         int klingons = game.totalKlingons();
-        scanner.setLines("PHA", "100");
-        game.step();
+        command(game, "PHA", "100");
         float shields = game.shields();
         assertTrue(shields < 1000);
-        scanner.setLines("PHA", "100");
-        game.step();
+        command(game, "PHA", "100");
         assertEquals(klingons - 1, game.totalKlingons());
+    }
+
+    private void command(Game game, String... command) {
+        scanner.setLines(command);
+        game.step();
     }
 
     private static class GameInputImpl implements GameInput {
